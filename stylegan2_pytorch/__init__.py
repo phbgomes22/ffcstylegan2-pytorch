8982e65958737de9c7f29a05910b484bd6caf873
Pedro Gomes <phbgomes@hotmail.com>
2023-08-31 11:30:22 -0300

    change init in folder

--- stylegan2_pytorch/__init__.py
+++ stylegan2_pytorch/__init__.py
@@ -1,1 +1,4 @@
-from stylegan2_pytorch.stylegan2_pytorch import Trainer, StyleGAN2, NanException, ModelLoader
+from .stylegan2_pytorch import *
+from .diff_augment import *
+from .tar_loader import *
+from .cli import *
