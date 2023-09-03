aaead2eecedc0b0f38d53e47ecce7fb93ad40d6e
Pedro Gomes <phbgomes@hotmail.com>
2023-09-03 19:57:43 -0300

    remove print layer from resizer

--- stylegan2_pytorch/ffc/resizer.py
+++ stylegan2_pytorch/ffc/resizer.py
@@ -4,13 +4,11 @@ Authors: Pedro Gomes
 
 import torch
 import torch.nn as nn
-from .print_layer import Print
 
 
 class Resizer(nn.Module):
     def __init__(self, debug = False):
         super(Resizer, self).__init__()
-        self.print_size = Print(debug=debug)
 
     def forward(self, x):
         output = x
@@ -19,6 +17,4 @@ class Resizer(nn.Module):
                 output = x[0]
             else:
                 output = torch.cat(list(x), dim=1)
-               # output = x.view(x.shape[0], -1, *x.shape[3:])
-                x = self.print_size(output)
         return output
